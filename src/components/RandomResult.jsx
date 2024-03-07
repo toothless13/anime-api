0df37956bcb3b74322f43abc21a20f5eb95e0c98
Tim <50870607+toothless13@users.noreply.github.com>
2024-03-07 14:05:58 +0000

    make Synopsis heading display conditionally

--- src/components/RandomResult.jsx
+++ src/components/RandomResult.jsx
@@ -9,7 +9,7 @@ const RandomResult = ({ result, imgUrl, link, synopsis, isLoading }) => {
         <h2 className="text-2xl font-bold">{result}</h2>
         {imgUrl !== undefined && <img src={imgUrl} alt="Image for anime or manga" className=" mt-4 rounded-md" />}
         {link !== undefined && <p className="my-4 mx-4 p-4 rounded-md border-2 border-green-800 cursor-pointer hover:border-pink-200 hover:text-white bg-gradient-to-r hover:from-green-800 hover:to-pink-300 drop-shadow-md"><a href={link} target="_blank" className="contents md:1/3 w-3/4">More Info - MyAnimeList</a></p>}
-        <h3 className="text-xl font-semibold">Synopsis</h3>
+        {synopsis !== undefined && <h3 className="text-xl font-semibold">Synopsis</h3>}
         <p className="my-4 w-3/4 md:1/3">{synopsis}</p>
       </div>
   )
